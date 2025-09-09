1d6308842217ecfc0f6ceba48057ad2d737d2d6c
Flavio Carvalho <flavsc.brz@gmail.com>
2025-09-09 14:44:56 -0300

    fix: hero knot import fixed

--- src/app/web/components/3D/HeroRightElement.tsx
+++ src/app/web/components/3D/HeroRightElement.tsx
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { useFrame, useThree } from '@react-three/fiber'; // CORRIGIDO: de "-" para "/"
+import { useFrame, useThree } from '@react-three/fiber';
 import * as THREE from 'three';
 
 export default function HeroRightElement() {
